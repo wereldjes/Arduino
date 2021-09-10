b1a4d1892237a51332b5813a2531102fa3bd0e0d
Dominique van Voorst <niehau@gmail.com>
2021-09-10 14:22:24 +0200

    Update Opdracht_2.ino

--- Opdracht_1/Opdracht_2.ino
+++ Opdracht_1/Opdracht_2.ino
@@ -11,16 +11,16 @@ void setup()
 void loop()
 {
 
-  if(digitalRead(button) && !check) {
-    check = 1;
-    if(digitalRead(led)){
-       digitalWrite(led, LOW); 
+  if(digitalRead(button) && !check) { //kijk of de knop is ingedrukt en of check op false staat
+    check = 1;                        //zet de check op 1 zodat de if statement niet herhaald kan worden
+    if(digitalRead(led)){             //kijk of de led al aan staat
+       digitalWrite(led, LOW);        //zet de led uit
     } else {
-        digitalWrite(led, HIGH);
+        digitalWrite(led, HIGH);      //zet de led anders aan
     }
   }
   
-  if(!digitalRead(button)) {
+  if(!digitalRead(button)) {          //kijk of de knop is uitgedrukt en zet de check weer op 0 zodat het if statement weer uitgevoerd kan worden bij een button press
     check = 0; 
   }
   
